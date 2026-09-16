90b43fcac25f1b5a07a381efc63e7ff68ac73b0c
hb718 <hb7131k@gmail.com>
2026-09-16 22:16:27 +1000

    Update server.js

--- js/server.js
+++ js/server.js
@@ -828,14 +828,6 @@ app.get("/", (req, res) => {
     );
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "login.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
