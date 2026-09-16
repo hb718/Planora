68b1c9ef49fe41e99d115274ef1008267d8ebd24
hb718 <hb7131k@gmail.com>
2026-09-16 22:09:32 +1000

    Update server.js

--- js/server.js
+++ js/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const db = new sqlite3.Database(
@@ -826,6 +826,12 @@ app.get("/", (req, res) => {
     res.sendFile(
         path.join(__dirname, "..", "login.html")
     );
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "login.html"));
 });
 
 const PORT = process.env.PORT || 3000;
